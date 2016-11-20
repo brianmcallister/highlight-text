3337e12058d6f0eeec19336454db6d99fdbfa2fc
Brian McAllister <brian@brianmcallister.com>
2016-11-19 19:15:36 -0500

    More terse.

--- src/lib/markup.js
+++ src/lib/markup.js
@@ -1,24 +1,21 @@
-const markup = function(text, boundaries, startTag = '', endTag = '') {
+const markup = (text, boundaries, startTag = '', endTag = '') => {
+  const tagLength = startTag.length + endTag.length;
+
   return boundaries.reduce((acc, next, index) => {
-    let offset;
-    let startIndex;
-    let endIndex;
+    // The only time we don't need to account for tag length is when we're
+    // marking up the first matched word.
+    const first = index === 0;
+    const offset = first ? 0 : tagLength * index;
+    const startIndex = first ? next[0] : next[0] + offset;
+    const endIndex = first ? next[1] : next[1] + offset;
 
-    if (index === 0) {
-      startIndex = next[0];
-      endIndex = next[1];
-    } else {
-      offset = (startTag.length + endTag.length) * index;
-      startIndex = next[0] + offset;
-      endIndex = next[1] + offset;
-    }
-
+    // Assemble the pieces.
     const start = acc.slice(0, startIndex);
     const word = acc.slice(startIndex, endIndex);
     const end = acc.slice(endIndex);
 
     return `${start}${startTag}${word}${endTag}${end}`;
-  }, text);
+  }, text)
 };
 
 module.exports = markup;
