75516304714858807e3d242936608ba6c48d4b4d
Brian McAllister <brian@brianmcallister.com>
2019-11-06 17:32:52 -0500

    Tweak build settings.

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    libraryTarget: 'commonjs2',
+    libraryTarget: 'umd',
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.json'],
